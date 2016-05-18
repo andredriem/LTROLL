59581f7e87e0c150166b4fae95289c7917e1c8dc
andredriem <andredriem@gmail.com>
2016-05-18 12:45:36 -0300

    blabla

---
+++ tokentest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Recursive LTROLL interpeter
+# God have mercy of our souls
+# by: André Driemeyer andredriem@gmail.com
+
+
+
+from tokenTree import TokenTree
+		
+		
+			
+
+
+if __name__ == '__main__':
+	test = TokenTree('if 30 then fn 3 / ! nat --> bool -> true else 0')
+	print test
+	
