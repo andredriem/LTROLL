c523ad6498a9b56bc1ac65b350b2cf402aa5f23e
Vilmar Dorneles Aprato Neto <Vilmar Dorneles Aprato Neto>
2016-06-09 16:00:01 -0300

    Sistemas de tipos update 0.1

--- Sistema_Tipos.py
+++ Sistema_Tipos.py
@@ -23,6 +23,10 @@ memoria = {
 
     }
 
+ if is_a_number(x):
+	print("xd")
+ else:
+    print(":(")   
 #======================================
 # Funções principais:
 # is_a_number => identifica se o tipo é inteiro
@@ -33,10 +37,11 @@ memoria = {
 #======================================
 def is_a_number(var):
     try:
-        if var == int(var):
+        if var == int(var) and var>= 0 :
             return True
     except Exception:
         return False
+        
 #======================================
 # Função is_a_bool(var)=> bool
 # onde var é uma variavel 
@@ -48,3 +53,29 @@ def is_a_bool(var):
     except Exception:
         return False
 #=======================================
+debug = [2, "+", 4]
+
+##if 3>=2 then 4 else 3
+
+
+def is_a_skip(var):
+    try:
+        if var == "skip":
+            return True
+    except Exception:
+        return False
+
+
+def is_a_fn(lista):
+
+    v1 = lista[0]
+    v2 = lista[1]
+    v3 = lista[2]
+        
+        if is_a_number(lista[3]) or lista[3] == lista[1]:
+            for i in xrange(3, len(lista), 1):
+                if (lista[i] == "+" or lista[i] == "-") and not((is_a_number(lista[i-1]) and is_a_number(lista[i+1]))) and not((lista[i-1]==lista[1]) and (lista[i+1]==lista[1])):
+                    
+
+def verificador_bem_tipada(expressao):
+    
